8d5ae50d3fb1fa896a841d1b80f57b4465db5424
szamocza <szamocza@vvv>
2017-11-09 16:06:40 +0100

    Add Randomness to Cloud

--- src/draw/handler/Draw.Cloud.js
+++ src/draw/handler/Draw.Cloud.js
@@ -1,3 +1,23 @@
+L.Util.Random = function(seed) {
+    this._seed = seed % 2147483647;
+    if (this._seed <= 0) this._seed += 2147483646;
+};
+
+/**
+ * Returns a pseudo-random value between 1 and 2^32 - 2.
+ */
+L.Util.Random.prototype.next = function () {
+    return this._seed = this._seed * 16807 % 2147483647;
+};
+
+
+/**
+ * Returns a pseudo-random floating point number in range [0, 1).
+ */
+L.Util.Random.prototype.nextFloat = function () {
+    // We know that result of next() will be 1 to 2147483646 (inclusive).
+    return (this.next() - 1) / 2147483646;
+};
 
 L.Cloud = L.Polygon.extend({
     options: {
@@ -15,6 +35,13 @@ L.Cloud = L.Polygon.extend({
         return area;
     },
 
+    redraw: function () {
+        if (this._map) {
+            this._updatePath();
+        }
+        return this;
+    },
+
     _updatePath: function () {
         var rings = this._parts;
 
@@ -22,13 +49,15 @@ L.Cloud = L.Polygon.extend({
             i, j, rn, len2, points, p,
         frillLen = this.options.frillLen,
         frillHeight = this.options.frillHeight;
+        var rand = new L.Util.Random(3);
 
         for (i = 0, rn = rings.length; i < rn; i++) {
             points = rings[i];
 
             var prevP;
             var sign = this._polygonArea(points) < 0 ? -1 : 1;
-            for (j = 0, len2 = points.length; j <= len2; j++) {
+            len2 = points.length;
+            for (j = 0; j <= len2; j++) {
                 p = points[j % len2];
                 if(j) {
                     var dx = p.x - prevP.x;
@@ -48,7 +77,9 @@ L.Cloud = L.Polygon.extend({
                     var nx = iy * frillHeight * sign;
                     var ny = -ix * frillHeight * sign;
                     for(; n; n--) {
-                        path += 'c ' + nx + ' ' + ny + ',' + (ix + nx) + ' ' + (iy + ny) + ',' + ix + ' ' + iy;
+                        var r1 = 0.5 + rand.nextFloat();
+                        var r2 = 0.5 + rand.nextFloat();;
+                        path += 'c ' + r1 * nx + ' ' + r1 * ny + ',' + (ix + r2 * nx) + ' ' + (iy + r2 * ny) + ',' + ix + ' ' + iy;
                     }
                 } else {
                     path += 'M' + p.x + ' ' + p.y;
